7c8fe39ae8d57cd23ef4337315593d73558e1e0a
arne.hassel <arne.hassel@gmail.com>
2017-11-14 18:10:18 +0100

    Enkel mock for layout av skjema

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
-import './App.css';
 import Comparer from './Comparer';
-
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to the app</h2>
+      <div>
+        <div className="jumbotron">
+          <div className="container">
+            <h2>Løftesammenligning</h2>
+          </div>
         </div>
-        <Comparer />
+        <div className="container">
+          <Comparer/>
+        </div>
       </div>
     );
   }
--- src/Comparer.tsx
+++ src/Comparer.tsx
@@ -3,7 +3,23 @@ import * as React from 'react';
 class Comparer extends React.Component {
   render() {
     return (
-      <div>Comparer!</div>
+      <div>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="form-group">
+              <label htmlFor="compareTextA">Løfter A</label>
+              <textarea id="compareTextA" className="form-control" rows={10}/>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="form-group">
+              <label htmlFor="compareTextB">Løfter B</label>
+              <textarea id="compareTextB" className="form-control" rows={10}/>
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-primary btn-block">Sammenlign</button>
+      </div>
     );
   }
 }
